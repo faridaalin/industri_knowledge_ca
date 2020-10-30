7fc95a2b0be693edce74c82ebaa3a8ede3e98069
Farida <farida.alin@yahoo.com>
2020-10-30 12:35:44 +0100

    thumbnail styling

--- gatsby-frontend/src/pages/blog.js
+++ gatsby-frontend/src/pages/blog.js
@@ -8,8 +8,6 @@ import styled from "styled-components/macro"
 export const Thumbnail = styled(Link)`
   margin-bottom: 2rem;
   width: 100%;
-  /* min-height: 251px;
-  max-height: 300px; */
 
   :hover {
     color: var(--whitesmoke--lightest);
@@ -19,20 +17,53 @@ export const Thumbnail = styled(Link)`
     }
   }
 
-  .gatsby-image-wrapper{
-    /* min-height: 200px;
+  .thumbnail__content {
+    padding-top: 1rem;
 
-    div {
-      padding-bottom: 0 !important;
-    } */
+    .title {
+      padding: 0.5rem 0;
+    }
+
+    .description {
+      padding-bottom: 1rem;
+    }
+    .date {
+      color: var(--grey--lightest);
+    }
   }
-  .date {
-    color: var(--grey--lightest);
+
+
+  @media all and (min-width:768px) {
+    display: flex;
+
+    .thumbnail__img {
+    flex: 1;
   }
+  .thumbnail__content {
+    flex: 2;
+    padding: 0 1rem;
+
+    .title {
+      padding-top: 0;
+    }
+
+    .description {
+      font-size: 1rem;
+      line-height: 1.2;
+      padding-top: 0.5rem;
+    }
+    .date {
+    font-size: 14px;
+    padding-top: 1rem;
+  }
+  }
+  }
+ 
 `
 export const Section = styled.section`
   max-width: 45rem;
   margin: 0 auto;
+  padding: 2.5rem 0;
 `
 
 const Blog = ({ data }) => {
@@ -45,16 +76,16 @@ const Blog = ({ data }) => {
             to={item.slug.current}
             className="thumbnail"
           >
-      
+            <div className="thumbnail__img">
             <Img 
                 style={{ maxHeight: "100%" }}
             fluid={item.mainImage.asset.fluid} 
             alt={item.mainImage.alt} />
-      
-
+            </div>
+  
             <div className="thumbnail__content">
               <h3 className="title">{item.title}</h3>
-              <p>{item.description}</p>
+              <p className="description">{item.description}</p>
               <p className="date">
                 <time>{item.publishedAt}</time>
               </p>
